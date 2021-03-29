bf87969efa6e2b457499967b1dbcf97a96aa0ba2
JK <u.sslooky@gmail.com>
2021-03-29 02:43:49 +0000

    feat: add localtunnel types as a dev dependency. Adds '@types/localtunnel' as a dev dependency. + Formats source.
    Signed-off-by: JK <u.sslooky@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cruise
+
 A VSCode extension that allows you to easily expose ports and more using Localtunnel.
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,3 @@
 import * as vscode from "vscode"
-import * as v8 from "v8"
-
-v8.serialize(v8.deserialize(x))
 
 export function activate(context: vscode.ExtensionContext) {}
